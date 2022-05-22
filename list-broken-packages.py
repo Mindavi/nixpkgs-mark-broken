d98a16fdc8ea5a259109b48bb42af4d70b54d63b
Rick van Schijndel <rol3517@gmail.com>
2022-05-22 13:35:59 +0200

    use multiprocessing for retrieving build results

--- list-broken-packages.py
+++ list-broken-packages.py
@@ -3,10 +3,11 @@
 import requests
 import datetime
 import sys
+import multiprocessing
 
 # Selecting a different jobset may be handy for debugging the script.
-#jobset = "nixpkgs/trunk"
-jobset = "nixpkgs/cross-trunk"
+jobset = "nixpkgs/trunk"
+#jobset = "nixpkgs/cross-trunk"
 #jobset = "patchelf/master"
 
 print("listing packages with build status")
@@ -38,19 +39,23 @@ all_builds_in_eval = builds.json()["builds"]
 print(f"number of builds: {len(all_builds_in_eval)}")
 
 # TODO(ricsch): Parallelize?
-for build in all_builds_in_eval:
-    build_result = requests.get(f"https://hydra.nixos.org/build/{build}", headers={"Accept": "application/json"})
+def print_build_result(build_id):
+    build_result = requests.get(f"https://hydra.nixos.org/build/{build_id}", headers={"Accept": "application/json"})
     try:
         job = build_result.json()["job"]
         status = build_result.json()["buildstatus"]
     except:
-        print(f"build {build} unknown status, {build_result}", file=sys.stderr)
-        continue
+        print(f"build {build_id} unknown status, {build_result}", file=sys.stderr)
+        return
     # status can be:
     #   None: not built yet
     #   0: success
     #   1: Build returned a non-zero exit code 
     #   2: indirect failure
     #   11: output limit exceeded
-    print(f"status {status},\tjob {job}")
+    print(f"status {status}, id {build_id}, job {job}")
+    sys.stdout.flush()
+
+pool = multiprocessing.Pool(500)
+pool.map(print_build_result, all_builds_in_eval)
 
