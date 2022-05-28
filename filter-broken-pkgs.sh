71f12c11f0d881ef5cce8c14817adee33e37ddcd
Rick van Schijndel <rol3517@gmail.com>
2022-05-28 15:03:04 +0200

    filter-broken-pkgs.sh: add filename argument

--- filter-broken-pkgs.sh
+++ filter-broken-pkgs.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash --pure -p ripgrep gawk
 
-tail +6 pkgs-with-status.txt | rg "status 1," | cut -d' ' -f6- | sort | awk '{ print "- [ ] " $0 }'
+if [[ $# -ne 1 ]]; then
+  echo "Invalid number of arguments"
+  echo "Usage: filter-broken-pkgs.sh <packages-with-status>"
+  exit 1
+fi
+
+file=$1
+tail +6 "$1" | rg "status 1," | cut -d' ' -f6- | sort
 
