0f8160a03460215c72c47340cbc16c994980aee0
Rick van Schijndel <rol3517@gmail.com>
2022-12-05 21:27:04 +0100

    mark_broken_v2: add broken mark to bottom, combine platforms, be smarter with ()

--- mark_broken_v2.py
+++ mark_broken_v2.py
@@ -23,12 +23,71 @@ denyAttrList = [
     "rubyPackages_",
 ]
 
-supportedPlatforms = [
-    "aarch64-linux",
-    "x86_64-linux",
-    "aarch64-darwin",
-    "x86_64-darwin",
-]
+platformsAndBrokenText = {
+    "aarch64-linux": "stdenv.isLinux && stdenv.isAarch64",
+    "x86_64-linux": "stdenv.isLinux && stdenv.isx86_64",
+    "aarch64-darwin": "stdenv.isDarwin && stdenv.isAarch64",
+    "x86_64-darwin": "stdenv.isDarwin && stdenv.isx86_64",
+}
+
+supportedPlatforms = list(platformsAndBrokenText.keys())
+
+shortPlatforms = {
+    "stdenv.isLinux": [ "x86_64-linux", "aarch64-linux" ],
+    "stdenv.isDarwin": [ "x86_64-darwin", "aarch64-darwin" ],
+}
+
+def numLeadingSpaces(input_str):
+    count = 0
+    for c in input_str:
+        if c != ' ':
+            return count
+        count += 1
+
+    return count
+
+def insertBrokenMark(file, brokenText, comment):
+    prev_line = None
+    shutil.copyfile(file, f'{file}.bak', follow_symlinks=False)
+    with open(file, "r") as input_file:
+        input_data = input_file.read()
+
+    move_broken_to_meta_bottom = True
+
+    output_lines = []
+
+    in_meta = False
+    meta_end = False
+    for line in input_data.splitlines():
+        line = line.rstrip()
+        # TODO(Mindavi): Decide if we want to replace the current line or move it to the bottom.
+        brokenline = 'broken =' in line
+        if brokenline:
+            # Assume this broken line terminates on the same line.
+            assert(';' in line)
+            # It's not really nice to move the broken line if an explanation of the brokenness is provided above it.
+            assert('#' not in prev_line)
+            continue
+        if meta_end:
+            meta_end = False
+        if 'meta =' in line:
+            in_meta = True
+        elif in_meta and '};' in line:
+            meta_end = True
+            in_meta = False
+        elif in_meta:
+            meta_indent = numLeadingSpaces(line)
+        if not meta_end and prev_line != None:
+            output_lines.append(prev_line)
+        elif meta_end:
+            output_lines.append(prev_line)
+            output_lines.append(f"{' ' * meta_indent}broken = {brokenText};{comment}")
+        prev_line = line
+    output_lines.append(prev_line)
+
+    with open(file, 'w') as output_file:
+        for line in output_lines:
+            print(line, file=output_file)
 
 def failMark(attr, message):
     print(f"{attr}: {message}", file=sys.stderr)
@@ -83,7 +142,7 @@ def attemptToMarkBroken(attr: str, platforms: Iterable[str], extraText = ""):
     alreadyMarkedPlatforms.sort()
     extraPlatforms = list(set(platforms) - set(alreadyMarkedPlatforms))
     if alreadyMarkedPlatforms == platforms or len(extraPlatforms) == 0:
-        #print(f"Package {attr} is already marked broken for all platforms listed {alreadyMarkedPlatforms}, not doing anything")
+        print(f"Package {attr} is already marked broken for all platforms listed {alreadyMarkedPlatforms}, not doing anything")
         return
 
     platforms = list(set(platforms + alreadyMarkedPlatforms))
@@ -92,35 +151,37 @@ def attemptToMarkBroken(attr: str, platforms: Iterable[str], extraText = ""):
     assert(len(platforms) <= len(supportedPlatforms))
 
     brokenText = ""
+    for [short, combinablePlatforms] in shortPlatforms.items():
+        platformsWithoutCombinablePlatforms = set(platforms) - set(combinablePlatforms)
+        if (len(platforms) - 2 == len(platformsWithoutCombinablePlatforms)):
+            if len(brokenText) > 0:
+                brokenText += " || "
+            brokenText += short
+            # Remove the platforms from the list of platforms to be considered.
+            platforms = list(filter(lambda item: item not in combinablePlatforms, platforms))
+    multiplePlatforms = len(platforms) > 1 or len(brokenText) > 0
     for platform in platforms:
         if len(brokenText) > 0:
             brokenText += " || "
-        if platform == "aarch64-linux":
-            brokenText += "\(stdenv.isLinux \&\& stdenv.isAarch64\)"
-        elif platform == "x86_64-linux":
-            brokenText += "\(stdenv.isLinux \&\& stdenv.isx86_64\)"
-        elif platform == "aarch64-darwin":
-            brokenText += "\(stdenv.isDarwin \&\& stdenv.isAarch64\)"
-        elif platform == "x86_64-darwin":
-            brokenText += "\(stdenv.isDarwin \&\& stdenv.isx86_64\)"
+        if multiplePlatforms:
+            brokenText += "("
+        brokenText += platformsAndBrokenText[platform]
+        if multiplePlatforms:
+            brokenText += ")"
 
     # TODO(mindavi): We could shorten (x86_64-x + aarch64-x) to stdenv.isX.
 
     if platforms == supportedPlatforms:
         brokenText = "true"
 
+    assert(brokenText != "")
     assert(not "#" in extraText and not "/" in extraText)
     comment = ""
     if extraText != "":
       comment = f"  # {extraText}"
 
     # insert broken attribute
-    subprocess.run([ "sed", "-i.bak", nixFile, "-r",
-        # Delete any old broken mark
-        "-e", "/^\s*broken\s*=.*$/d",
-        # Insert new broken mark in meta 
-        "-e", "s/(\\s*)meta\\s*=.*\\{/&\\n\\1  broken = " + brokenText + ";" + comment + "/" ]
-    )
+    insertBrokenMark(nixFile, brokenText, comment)
 
     if filecmp.cmp(nixFile, f"{nixFile}.bak", shallow=False):
         shutil.move(f"{nixFile}.bak", nixFile)
@@ -132,7 +193,7 @@ def attemptToMarkBroken(attr: str, platforms: Iterable[str], extraText = ""):
         nixMarkedCheck = subprocess.run([ "nix-instantiate", "--eval", "--json", "-E", f"with import ./. {{ localSystem = \"{platform}\"; }}; {attr}.meta.broken" ], capture_output=True)
         if nixMarkedCheck.returncode != 0:
             shutil.move(f"{nixFile}.bak", nixFile)
-            failMark(attr, f"Failed to check {attr}.meta.broken for platform {platform}")
+            failMark(attr, f"Failed to check {attr}.meta.broken for platform {platform}: {nixMarkedCheck.stderr.decode('utf-8')}")
             return
         markedSuccessfully = json.loads(nixMarkedCheck.stdout.decode('utf-8'))
         if not markedSuccessfully:
